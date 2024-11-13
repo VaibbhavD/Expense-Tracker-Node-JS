5ad5c81b92a3fcac8e67b4c0436ee7a62350d676
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-11-13 11:15:18 +0530

    definr router and routes path and elements

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUp from "./components/SignupPage";
+import LoginPage from "./components/LoginPage";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <SignUp />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
