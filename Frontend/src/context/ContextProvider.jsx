88221218b7b17d2a3df53118724baadd2f99794a
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-11-16 14:51:09 +0530

    Add token state

--- Frontend/src/context/ContextProvider.jsx
+++ Frontend/src/context/ContextProvider.jsx
@@ -3,13 +3,16 @@ import Context from "./store";
 
 const ContextProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, settoken] = useState();
 
   const login = () => {
     setIsLoggedIn(true);
   };
 
   return (
-    <Context.Provider value={{ login, isLoggedIn, setIsLoggedIn }}>
+    <Context.Provider
+      value={{ login, isLoggedIn, setIsLoggedIn, token, settoken }}
+    >
       {children}
     </Context.Provider>
   );
