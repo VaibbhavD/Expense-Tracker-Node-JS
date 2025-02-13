918bf87965cc4aecaf1a3643a53e9751050d2a44
Vaibhav D <vaibhavdhamanage12@gmail.com>
2025-02-13 15:32:02 +0530

    add reset pass

--- Frontend/src/components/LoginPage.jsx
+++ Frontend/src/components/LoginPage.jsx
@@ -25,19 +25,19 @@ const LoginPage = () => {
     try {
       const res = await axios.post("http://localhost:4000/login", { formData });
   
-      if (res.status === 200) {
-        login();
-        alert(res.data.message);
-        localStorage.setItem("token", res.data.user.token);
-        settoken(res.data.user.token);
-        navigate("/expense");
+    if (res.status === 200) {
+      login();
+      alert(res.data.message);
+      localStorage.setItem("token", res.data.user.token);
+      settoken(res.data.user.token);
+      navigate("/expense");
       }
     } catch (error) {
       // Handle incorrect password or user not found
       if (error.response) {
         alert(error.response.data.message);
         console.error("Login Error:", error.response.data.message);
-      } else {
+    } else {
         alert("An unexpected error occurred. Please try again.");
         console.error("Unexpected Error:", error);
       }
@@ -65,58 +65,58 @@ const LoginPage = () => {
     console.error(error);
     alert(error.response.data.message);
   }
-};
+  };
 
 
   return (
     <>
       {!resetPassword && (
-        <div className="flex items-center justify-center min-h-screen bg-gray-100">
-          <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
-            <h2 className="text-2xl font-bold mb-6 text-gray-700 text-center">
-              Login
-            </h2>
-            <form onSubmit={handleSubmit} className="space-y-4">
-              {/* Email Field */}
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  required
-                  className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                  placeholder="Enter your email"
-                />
-              </div>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
+        <h2 className="text-2xl font-bold mb-6 text-gray-700 text-center">
+          Login
+        </h2>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          {/* Email Field */}
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+              className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              placeholder="Enter your email"
+            />
+          </div>
 
-              {/* Password Field */}
-              <div>
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Password
-                </label>
-                <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  value={formData.password}
-                  onChange={handleChange}
-                  required
-                  minLength="6"
-                  className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                  placeholder="Create a password"
-                />
-              </div>
+          {/* Password Field */}
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+              minLength="6"
+              className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+              placeholder="Create a password"
+            />
+          </div>
               <div
                 className="w-full flex justify-end text-sm text-blue-500 cursor-pointer"
                 onClick={ShowResetPassword}
@@ -124,29 +124,29 @@ const LoginPage = () => {
                 Forget Password?
               </div>
 
-              {/* Submit Button */}
+          {/* Submit Button */}
               <div className="">
-                <button
-                  type="submit"
-                  className="w-full py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                >
-                  Login
-                </button>
-              </div>
-              <div className="mt-1 text-sm text-center text-gray-500">
-                <p>
-                  New User?{" "}
-                  <span
-                    className="text-blue-500 cursor-pointer"
-                    onClick={() => navigate("/signup")}
-                  >
-                    Sign up
-                  </span>
-                </p>
-              </div>
-            </form>
+            <button
+              type="submit"
+              className="w-full py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            >
+              Login
+            </button>
           </div>
-        </div>
+          <div className="mt-1 text-sm text-center text-gray-500">
+            <p>
+              New User?{" "}
+              <span
+                className="text-blue-500 cursor-pointer"
+                onClick={() => navigate("/signup")}
+              >
+                Sign up
+              </span>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
       )}
       {/* Reset Password */}
       {resetPassword && (
