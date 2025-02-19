aa6f8f73580e40c99d621b50a1c91d40ac86544a
Vaibhav D <vaibhavdhamanage12@gmail.com>
2025-02-19 10:08:26 +0530

    db module update

--- Backend/models/db.js
+++ Backend/models/db.js
@@ -2,11 +2,23 @@ const { Sequelize, DataTypes } = require("sequelize");
 const { v4: uuidv4 } = require("uuid");
 require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, "root", process.env.DB_PASSWORD, {
-  host: "localhost",
-  dialect: "mysql",
-  port: 3300,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    port: process.env.DB_PORT,
+    logging: false, // Disable logging in production
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  }
+);
 
 const User = sequelize.define("users", {
   id: {
@@ -58,8 +70,8 @@ const Expenses = sequelize.define("expenses", {
     type: DataTypes.INTEGER,
     allowNull: false,
     references: {
-      model: "users", // Name of the target table
-      key: "id", // Column name in the target table
+      model: User,
+      key: "id",
     },
     onUpdate: "CASCADE",
     onDelete: "CASCADE",
@@ -75,7 +87,7 @@ const Order = sequelize.define("order", {
   },
   paymentid: {
     type: DataTypes.STRING,
-    allowNull: true, // This can be null initially (before payment is completed)
+    allowNull: true,
   },
   orderid: {
     type: DataTypes.STRING,
@@ -100,7 +112,7 @@ const ForgotPasswordRequest = sequelize.define(
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: "users",
+        model: User,
         key: "id",
       },
     },
